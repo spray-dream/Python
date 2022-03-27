d1e455660a1a0c58ad311c519d78bdd20dbcb71f
spray_dream <spraydream58@gmail.com>
2022-03-27 22:01:07 +0800

    Default Changelist

--- 数据分析/numpy/004-数组中存储日期类型.py
+++ 数据分析/numpy/004-数组中存储日期类型.py
@@ -5,6 +5,20 @@
 # @Software: PyCharm
 import numpy as np
 
+# 格式固定xxxx-xx-xx
 data = ['2011-01-01', '2011', '2011-02', '2012-01-02', '2012-02-01 10:10:00']
 a = np.array(data)
-print(a, a.dtype)
+print(a, a.dtype)    # ['2011-01-01' '2011' '2011-02' '2012-01-02' '2012-02-01 10:10:00'] <U19
+# <U19是字节序    2012-02-01 10:10:00是19个字符
+
+# 转换数据类型为M8[D]
+a = a.astype('M8[D]')
+print(a, a.dtype)    # ['2011-01-01' '2011-01-01' '2011-02-01' '2012-01-02' '2012-02-01'] datetime64[D]
+# M8表示datetime64数据类型,D表示时间的精确度
+
+# s表示精确到秒
+# a = a.astype('M8[s]')
+# print(a, a.dtype)
+
+# 还可以做运算
+print(a[2] - a[1])    # 2678400 seconds
---
+++ 数据分析/numpy/005-类型字符码.py
@@ -0,0 +1,23 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/3/27 21:47
+# @Author : spray_dream
+# @File : 005-类型字符码.py
+# @Software: PyCharm
+import numpy as np
+
+"""
+类型               字符码
+bool               ?
+int8/16/32/63      i1/12/14/18
+uint8/16/32/64     u1/u2/u4/u8
+float16/32/64      f2/f4/f8
+complex64/128      c8/c16
+str_               U<字符数>
+datetime64         M8[Y] M8[M] M8[D] M8[h] M8[m] M8[s]
+
+类型在规定时可以直接写类型,也可以写字符码
+"""
+
+"""
+字节序
+"""
